73a88dee3abe1372a366e94809af66705d1e7828
Eric <zixia@umail.ucsb.edu>
2018-07-27 18:07:53 -0700

    adding user table and collection table

--- create.sql
+++ create.sql
@@ -1,24 +1,39 @@
-CREATE TABLE podcast(
+CREATE TABLE podcast_list(
 	id int unique,
 	itunes_link varchar(100) NOT NULL,
-	podcast varchar(50),
+	podcast varchar(100),
 	img varchar(100),
 	category varchar(50),
 	language varchar(50),
 	rating varchar(50),
 	reviews varchar(50),
 	url varchar(100),
-	api_data TEXT NOT NULL,
+	api_data varchar(4096),
 	PRIMARY KEY (id)  -- id must be unique
-)
+);
 
 CREATE TABLE episode(
-       id int unique NOT NULL,
-       podcast_id int NOT NULL,
-       podcast varchar(50),
-       episode varchar(50),
-       release_date varchar(20), #DATE NOT NULL FORMAT 'MM/DD/YY' DEFAULT DATE '01/01/2001',
-       info varchar(200),
-       PRIMARY KEY (id),
-       FOREIGN KEY (podcast_id) REFERENCES podcast(id)
+	podcast_id int NOT NULL,
+	podcast varchar(50),
+	episode varchar(50),
+	release_date varchar(20), #DATE NOT NULL FORMAT 'MM/DD/YY' DEFAULT DATE '01/01/2001',
+	info varchar(200),
+	FOREIGN KEY (podcast_id) REFERENCES podcast_list(id)
+);
+
+CREATE TABLE user(
+	uid smallint unsigned not null auto_increment,
+	fname varchar(50),
+	lname varchar(50),
+	email varchar(50),
+	created_at int(11) DEFAULT NULL,
+  	updated_at int(11) DEFAULT NULL,
+	PRIMARY KEY (uid)
+) ENGINE=InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+CREATE TABLE user_collection(
+	uid smallint unsigned NOT NULL,
+	pid int NOT NULL,
+	FOREIGN KEY (uid) REFERENCES user(uid),
+	FOREIGN KEY (pid) REFERENCES podcast_list(id)
 );
