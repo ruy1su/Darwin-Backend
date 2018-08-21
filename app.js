7476931d3aa938177307775265f3eabd8a49957b
Eric <zixia@umail.ucsb.edu>
2018-08-21 16:14:58 -0700

    adding graph into memory for recommendation system. Thanks to Don Lee

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var express = require('express');
 var escapeJSON = require('escape-json-node');
 var bodyParser = require('body-parser')
 var app = express();
+const RecommendationEngine = require('./graph.js');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -15,7 +16,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 var connection = mysql.createConnection({
     host     : 'aam2629vgw55ee.czd1gxziytnq.us-east-2.rds.amazonaws.com',
     user     : 'eric',
-    password : 'Weng950702',
+    password : '1q2w3eDarwin',
     port     : '3306',
     database : 'darwin',
     multipleStatements: true
@@ -29,6 +30,76 @@ connection.connect(function(err) {
     console.log('Connected to database.');
 });
 
+// Load User into Graph
+
+var users = []
+var links = []
+var receng = new RecommendationEngine('darwin');
+// receng.load_graph('/home/ec2-user/Darwin-Backend/graph.ugd');
+
+// console.log("here")
+connection.query(`Select uid, fname, lname FROM user;`, function(error, rows, fields){
+        if(error){
+            console.log('Error in the query');
+        }
+        else{
+            console.log('Successfull query');
+            users = rows
+        }
+    });
+// receng.load_users(users)
+connection.query(`Select uid, pid FROM user_collection;`, function(error, rows, fields){
+        if(error){
+            console.log('Error in the query');
+        }
+        else{
+            console.log('Successfull query');
+            links = rows
+        }
+    });
+// receng.load_links(links)
+
+function task1 () {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      receng.load_graph('/home/ec2-user/Darwin-Backend/graph.ugd');
+      resolve('done');
+    }, 5000);
+  });
+}
+
+/**
+ * 第二个任务
+ */
+function task2 () {
+
+  return new Promise(resolve => {
+    setTimeout(() => {
+      receng.load_users(users)
+      resolve('done');
+    }, 5000)
+  });
+}
+
+/**
+ * 第三个任务
+ */
+function task3 () {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      receng.load_links(links)
+      resolve('done');
+    }, 5000);
+  });
+}
+
+async function allTasks () {
+  await task1();
+  await task2();
+  await task3();
+}
+
+allTasks();
 // POST API ---------------------------------------------------------------------------------------------------------------------------------------
 
 app.post("/create_user", (req, res) => {
@@ -45,10 +116,108 @@ app.post("/create_user", (req, res) => {
     });
 });
 
+app.post("/create_collection", (req, res) => {
+    var uid=req.body.uid;
+    var pid=req.body.pid;
+    console.log(req.body)
+    connection.query(`INSERT INTO user_collection (uid, pid) VALUES ("${uid}", "${pid}")`, function(err, result){
+        if(err) {return res.send(err);}
+        else{
+            console.log("1 collection inserted");
+            res.send("Success");
+        }
+    });
+});
+
+// DELETE API ---------------------------------------------------------------------------------------------------------------------------------------
+app.delete('/delete_usr_collection/:uid/:pid',function(req,res){
+     var uid = (req.params.uid)
+     var pid = (req.params.pid)
+     console.log(uid, pid)
+     connection.query(`DELETE FROM user_collection WHERE uid = ${uid} and pid = ${pid} ;`, function(err, result){
+        if(err) {console.log('Error in the query');}
+        else{
+            console.log("1 collection deleted");
+            res.send("Success");
+        }
+    });
+});
+
+// GET API ---------------------------------------------------------------------------------------------------------------------------------------
+
+// Login With Username and Password
+app.get("/login_request/:username/:password", function (req, res){
+    username = req.params.username;
+    password = req.params.password;
+    console.log(req.body)
+    connection.query(`Select uid FROM user where username = "${username}" and password = "${password}";`, function(error, rows, fields){
+        if(error){
+            console.log('Error in the query');
+        }
+        else{
+            console.log('Successfull query');
+            res.send(rows[0]);
+        }
+    });
+});
+
+// Login Success With Facebook Account
+app.get("/login/:email", function (req, res){
+    search_key = req.params.email;
+    console.log(req.body)
+    connection.query(`Select uid FROM user where email = "${search_key}";`, function(error, rows, fields){
+        if(error){
+            console.log('Error in the query');
+        }
+        else{
+            console.log('Successfull query');
+            res.send(rows[0]);
+        }
+    });
+});
+
+app.get("/load_user_coll/:uid", function (req, res){
+    search_key = req.params.uid;
+    console.log(search_key)
+    connection.query(`Select id, api_data FROM podcast_list where id in (select pid from user_collection where uid = "${search_key}");`, function(error, rows, fields){
+        if(error){
+            console.log('Error in the query');
+        }
+        else{
+            console.log('Successfull query');
+            var resultJsonList = [];
+            for (i = 0; i < rows.length; i++) { 
+                resultJson = new Object()  
+                var id = rows[i]['id']
+                var raw = rows[i]['api_data']
+                raw = raw.split("=>").join(":");
+                var jsData = JSON.parse(raw)
+                var parsedData = jsData['results']
+
+                resultJson['coverArtURL'] = parsedData[0]['artworkUrl600']
+                resultJson['artist'] = parsedData[0]['artistName']
+                resultJson['title'] = parsedData[0]['collectionName']
+                resultJson['pid'] = id
+                if(!parsedData[0]['feedUrl']){
+                    resultJson['mediaURL'] = parsedData[0]['artworkUrl600']
+                }
+                else{
+                    resultJson['mediaURL'] = parsedData[0]['feedUrl']
+                }
+
+                console.log(resultJson)
+
+                resultJsonList.push(resultJson)
+            }
+            res.send(resultJsonList);
+        }
+    });
+});
+
 // GET API ---------------------------------------------------------------------------------------------------------------------------------------
 // Home Page Api
 app.get('/api_home/', function (req,res) {
-    connection.query("SELECT id, api_data FROM podcast_list limit 15", function(error, rows, fields){
+    connection.query("SELECT id, api_data FROM podcast_list where id in (3, 30, 31, 34,12,14,17,18,19,20,25,26,29)", function(error, rows, fields){
        if(error){
            console.log('Error in the query');
        }
@@ -63,12 +232,12 @@ app.get('/api_home/', function (req,res) {
                 var jsData = JSON.parse(raw)
                 var parsedData = jsData['results']
 
-                resultJson['coverArtURL'] = parsedData[0]['artworkUrl100']
+                resultJson['coverArtURL'] = parsedData[0]['artworkUrl600']
                 resultJson['artist'] = parsedData[0]['artistName']
                 resultJson['title'] = parsedData[0]['collectionName']
                 resultJson['pid'] = id
                 if(!parsedData[0]['feedUrl']){
-                    resultJson['mediaURL'] = parsedData[0]['artworkUrl100']
+                    resultJson['mediaURL'] = parsedData[0]['artworkUrl600']
                 }
                 else{
                     resultJson['mediaURL'] = parsedData[0]['feedUrl']
@@ -118,7 +287,8 @@ app.get('/api_episode/:pid/', function (req,res) {
             for (i = 0; i < rows.length; i++) { 
                 rows[i]['eid'] = 0
                 rows[i]['artist'] = " "
-                rows[i]['mediaURL'] = "http://is5.mzstatic.com/image/thumb/Music6/v4/ca/82/2f/ca822f13-f1f7-dd8c-957d-a95a7b43501c/source/100x100bb.jpg"
+                rows[i]['mediaURL'] = "http://"
+                // http://feeds.soundcloud.com/stream/262465031-a-bomb-audio-episode-1-fight-with-monsters.mp3
             }
             
             res.send(rows);
@@ -135,7 +305,7 @@ app.get('/api_pc_epsd/:podcast_name/', function (req,res) {
        }
        else{
            console.log('Successfull query');
-       console.log(rows);
+           console.log(rows);
            res.send(rows);
        }
     });
@@ -145,7 +315,7 @@ app.get('/api_pc_epsd/:podcast_name/', function (req,res) {
 app.get('/api_search/:a?/', function (req,res) {
     search_key = req.params.a;
 
-    var sql = `SELECT api_data FROM podcast_list where podcast LIKE '%${search_key}%' `
+    var sql = `SELECT id, api_data FROM podcast_list where podcast LIKE '%${search_key}%' `
     console.log(sql)
     connection.query(sql, search_key, function(error, rows, fields){
        if(error){
@@ -156,6 +326,7 @@ app.get('/api_search/:a?/', function (req,res) {
            var resultJsonList = [];
             for (i = 1; i < rows.length; i++) { 
                 resultJson = new Object()  // init the new json object for return 
+                var id = rows[i]['id']
                 var raw = rows[i]['api_data']
                 // console.log(raw+"-------------\r\n");
                 raw = raw.split("=>").join(":");
@@ -170,8 +341,8 @@ app.get('/api_search/:a?/', function (req,res) {
                     // return res.status(400).send('malformed request: ' + raw);
                 }
                 var parsedData = jsData['results']
-
-                resultJson['coverArtURL'] = parsedData[0]['artworkUrl100']
+                resultJson['pid'] = id
+                resultJson['coverArtURL'] = parsedData[0]['artworkUrl600']
                 resultJson['artist'] = parsedData[0]['artistName']
                 resultJson['title'] = parsedData[0]['collectionName']
                 resultJson['duration'] = 0
@@ -183,46 +354,69 @@ app.get('/api_search/:a?/', function (req,res) {
     });
 });
 
-//home page: TRENDING
-app.get('/api_trending/', function (req,res) {
-    connection.query("SELECT api_data FROM podcast_list where id = 120033 or id = 116886 or id = 46300 or id = 59181;", function(error, rows, fields){
-       if(error){
-           console.log('Error in the query');
-       }
-       else{
+
+// Search Episode with Podcast Id
+app.get('/api_episode_trending/', function (req,res) {
+    search_key = req.params.pid;
+    connection.query(`SELECT podcast_id as pid, podcast, episode as title, release_date, info FROM episode where id = 1600 or id = 1700 or id = 590;`, function(error, rows, fields){
+        if(error){
+            console.log('Error in the query');
+        }
+        else{
             console.log('Successfull query');
-            showRes(res, rows);
-       }
+            var resultJsonList = [];
+            for (i = 0; i < rows.length; i++) { 
+                rows[i]['eid'] = 0
+                rows[i]['artist'] = " "
+                rows[i]['mediaURL'] = "http://is5.mzstatic.com/image/thumb/Music6/v4/ca/82/2f/ca822f13-f1f7-dd8c-957d-a95a7b43501c/source/100x100bb.jpg"
+            }
+            
+            res.send(rows);
+        }
     });
 });
 
-//home page: Your Friends are listen to
-app.get('/api_friends/', function (req,res) {
-    connection.query("SELECT api_data FROM podcast_list where id = 42576 or id = 128946 or id = 128970 or id = 129557;", function(error, rows, fields){
+//home page: TRENDING
+app.get('/api_trending/', function (req,res) {
+    connection.query("SELECT podcast_list.api_data, episode.podcast_id as pid, episode.podcast, episode.episode as title, episode.release_date, episode.info, episode.id as eid FROM episode join podcast_list ON episode.podcast_id = 131 and episode.podcast_id = podcast_list.id limit 4;", function(error, rows, fields){
        if(error){
            console.log('Error in the query');
        }
        else{
             console.log('Successfull query');
-            showRes(res, rows);
+            // res.send(rows);
+            var resultJsonList = [];
+            for (i = 0; i < rows.length; i++) { 
+                // resultJson = new Object()  // init the new json object for return 
+                var raw = rows[i]['api_data']
+                // console.log(raw+"-------------\r\n");
+                raw = raw.split("=>").join(":");
+                raw = raw.replace(/(\r\n\t|\n|\r\t)/gm,"");
+                // console.log(raw+"-------------\r\n");
+
+                try {
+                    var jsData = JSON.parse(raw);
+                } catch(e) {
+                    console.log('malformed request', raw);
+                    console.log(e);
+                    // return res.status(400).send('malformed request: ' + raw);
+                }
+                var parsedData = jsData['results']
+                rows[i]['artist'] = parsedData[0]['artistName']
+                rows[i]['coverArtURL'] = parsedData[0]['artworkUrl600']
+                rows[i]['mediaURL'] = parsedData[0]['feedUrl']
+
+                delete rows[i]['api_data']
+                // resultJsonList.push(resultJson)
+            }
+            res.send(rows);
+            // showRes(res, rows);
        }
     });
 });
 
 app.get('/api_up_next/', function (req,res) {
     connection.query("SELECT api_data FROM podcast_list where id = 123761 or id = 115049 or id = 113522;", function(error, rows, fields){
-       if(error){
-           console.log('Error in the query');
-       }
-       else{
-            console.log('Successfull query');
-            showRes(res, rows);
-       }
-    });
-});
-
-app.get('/api_play_list/', function (req,res) {
-    connection.query("SELECT api_data FROM podcast_list where id = 140170 or id = 44218 or id = 58604 or id = 129923 or id = 130183 or id = 130239;", function(error, rows, fields){ // or id = 138979
        if(error){
            console.log('Error in the query');
        }
@@ -267,7 +461,6 @@ app.use(function(req, res, next) {
     res.status(404);
     res.send('404: File Not Found');
 });
-
 
 // Listen to port other than 80
 app.listen(5000, () => console.log('Example app listening on port 3000!'));
---
+++ graph.js
@@ -0,0 +1,205 @@
+// // // import DataFrame from '../node_modules/dataframe-js';
+// // // import DataFrame, { Row } from 'dataframe-js';
+
+// const http = require('http');
+
+// const hostname = '127.0.0.1';
+// const port = 3000;
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World!\n');
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+
+// let ug = require('ug');
+// let graph = new ug.Graph();
+
+// let users = {
+// 	user_1: graph.createNode('user', {name: "Don Lee"}),
+// 	user_2: graph.createNode('user', {name: "Zixia Wang"}),
+// 	user_3: graph.createNode('user', {name: "Himank Sharma"}),
+// 	user_4: graph.createNode('user', {name: "Xiao"})
+// };
+
+// let podcasts = { 
+// 	podcast_1: graph.createNode('podcast', {name: 'A&E Radio!!!'}),
+// 	podcast_2: graph.createNode('podcast', {name: "A&G's Picture This!"}),
+// 	podcast_3: graph.createNode('podcast', {name: "A-Birding on a Bronco by MERRIAM, Florence A."})
+// };
+
+// graph.createEdge('likes').link(users.user_1, podcasts.podcast_1).setDistance(1);
+// graph.createEdge('likes').link(users.user_2, podcasts.podcast_1).setDistance(1);
+// graph.createEdge('likes').link(users.user_2, podcasts.podcast_2).setDistance(1);
+// graph.createEdge('likes').link(users.user_2, podcasts.podcast_3).setDistance(1);
+// graph.createEdge('likes').link(users.user_3, podcasts.podcast_3).setDistance(1);
+// graph.createEdge('likes').link(users.user_4, podcasts.podcast_3).setDistance(1);
+
+// graph.createEdge('listened').link(users.user_4, podcasts.podcast_3).setDistance(2);
+
+
+// let results = graph.closest(
+// 	graph.nodes('user').query().first(),
+// 	{
+// 		compare: function(node) {
+// 			return node.entity == 'podcast';
+// 		},
+// 		count: 2
+// 	}
+// );
+
+// results.forEach( function(result){
+// 	console.log(result.end().properties)
+// });
+
+
+// let ug = require('ug');
+// let graph = new ug.Graph();
+
+// let classification = graph.createNode('classification', {name: 'Sharing Economy'});
+
+// let corps = {
+//   uber: graph.createNode('corporation', {name: 'Uber'}),
+//   storefront: graph.createNode('corporation', {name: 'Storefront'}),
+//   airbnb: graph.createNode('corporation', {name: 'AirBnB'})
+// };
+
+// let industries = {
+//   vc: graph.createNode('industry', {name: 'Venture Capital'}),
+//   hospitality: graph.createNode('industry', {name: 'Hospitality'}),
+//   taxi: graph.createNode('industry', {name: 'Taxi'})
+// };
+
+// graph.createEdge('business_model').link(corps.uber, classification);
+// graph.createEdge('business_model').link(corps.airbnb, classification);
+// graph.createEdge('business_model').link(corps.storefront, classification);
+// graph.createEdge('emotion', {type: 'happy'}).link(industries.vc, classification);
+// graph.createEdge('emotion', {type: 'sad'}).link(industries.hospitality, classification);
+// graph.createEdge('emotion', {type: 'sad'}).link(industries.taxi, classification);
+
+// res = graph.closest(
+//   graph.nodes('classification').query().first(), // grab Sharing Economy node
+//   {
+//     compare: function(node) {
+//       // forget industries and uber!
+//       return node.entity !== 'industry' && node.get('name') !== 'Uber';
+//     },
+//     direction: -1 // only track nodes that feed in to this one
+//   }
+// );
+// console.log(res);
+var mysql = require('mysql');
+var ug = require('ug');
+var parse = require('csv-parse');
+var stream = require('csv-stream')
+var request = require('request')
+var fs = require('fs')
+var DataFrame = require('dataframe-js').DataFrame
+
+// var connection = mysql.createConnection({
+//     host     : 'aam2629vgw55ee.czd1gxziytnq.us-east-2.rds.amazonaws.com',
+//     user     : 'eric',
+//     password : 'Weng950702',
+//     port     : '3306',
+//     database : 'darwin',
+//     multipleStatements: true
+//     });
+// connection.connect(function(err) {
+//     if (err) {
+//        console.error('Database connection failed: ' + err.stack);
+//        return;
+//     }
+//     console.log('Connected to database.');
+// });
+
+
+class RecommendationEngine {
+	constructor (name) {
+		this.name = name;
+		this.graph = new ug.Graph();
+	}
+
+	load_users (users) {
+		var that = this;
+		for (var i = 0; i < users.length; i++) { 
+		    that.graph.createNode('user', users[i]);
+		}
+		console.log(users);
+	}
+
+	load_links (links){
+		var that = this;
+		var uid = 0
+		var pid = 0
+		for (var i = 0; i < links.length; i++) {
+			uid = links[i]["uid"]
+			pid = links[i]["pid"]
+			var unode = that.graph.nodes('user').query().filter({uid__is: uid}).units()[0];
+			console.log(unode)
+			var pnode = that.graph.nodes('podcast').query().filter({id__is: pid.toString()}).units()[0];
+			console.log(pnode,pid)
+			that.graph.createEdge('collection').link(unode, pnode).setDistance(1);
+		}
+		console.log(that.graph.edges('collection').query().units());
+	}
+
+	load_podcasts (podcasts_file_path){
+		var that = this;
+		var csvStream = stream.createStream();
+		var attributes;
+		fs.createReadStream(podcasts_file_path).pipe(csvStream).on('error', function(err){
+			console.error(err);
+		}).on('header', function(columns) {
+			console.log(columns);
+			attributes = columns;
+		}).on('data', function(data) {
+			console.log(data);
+			that.graph.createNode('podcast', data);
+		}).on('end', function(end) {
+			that.graph.save('/Users/don/Documents/Darwin/graph/graph.ugd', function(){
+				console.log('done');
+			})
+			// do nothing
+		})
+	}
+
+	load_graph (graph_path) {
+		var that = this;
+		console.log("In function")
+		this.graph.load(graph_path, function(err){
+			// do nothing
+			if (err) {
+				console.log('Error message:' + err);
+			} else {
+				console.log(that.graph.nodes('podcast').query().units()[0])
+			}
+		})
+	}
+
+	save_graph (graph_path) {
+		var that = this;
+		this.graph.save(graph_path, function(err){
+			// do nothing
+			if (err) {
+				console.log('Error message:' + err);
+			} else {
+				console.log(that.graph)
+			}
+		})
+	}
+}
+
+var receng = new RecommendationEngine('test');
+// console.log(receng.graph)
+module.exports = RecommendationEngine;
+// receng.load_podcasts('/Users/don/Documents/Darwin/data/itunes-podcast-details.csv');
+// receng.load_podcasts('/Users/don/Documents/Darwin/data/test.csv')
+// receng.save_graph('/Users/don/Documents/Darwin/graph/graph.ugd');
+// receng.load_graph('/Users/don/Documents/Darwin/graph/graph.ugd');
+// console.log(receng.graph)
+
